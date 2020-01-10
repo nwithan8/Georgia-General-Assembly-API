d1d31f1d1b4c48111a171afc805f6e08808efbcd
Nate Harris <n8gr8gbln@gmail.com>
2020-01-10 11:51:42 -0500

    Download link

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Nate Harris',                   # Type in your name
   author_email = 'n8gr8gbln@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/nwithan8/Georgia-General-Assembly-API',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/nwithan8/Georgia-General-Assembly-API/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['Georgia', 'API', 'government', 'general assembly', 'bills', 'laws', 'lawmakers', 'legislation', 'vote'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'zeep',

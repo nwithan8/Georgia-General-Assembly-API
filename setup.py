eaf2005e24bab53739e339ea9e95e48e86f9d9df
Nate Harris <n8gr8gbln@gmail.com>
2020-01-10 11:50:22 -0500

    Version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'gga',         # How you named your package folder (MyLib)
   packages = ['gga'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='GNU GPLv3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Interact with the Georgia General Assembly's website data',   # Give a short description about your library
   author = 'Nate Harris',                   # Type in your name

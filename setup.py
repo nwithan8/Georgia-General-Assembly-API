16c282de2fcf2055e20c0283405c00577a8dd164
Nate Harris <n8gr8gbln@gmail.com>
2020-01-10 11:56:12 -0500

    Fixed license syntax

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
